b350848b6f1ff78c2b218495a2003a8c4ba40982
uni-mairata <mhirata3@gatech.edu>
2026-04-08 01:23:56 -0400

    remove unused project

--- ui/file_list_gen.cmake
+++
@@ -1,7 +0,0 @@
-list(
-  APPEND
-  LV_EDITOR_PROJECT_SOURCES
-  ${CMAKE_CURRENT_LIST_DIR}/screens/screen_feedback_gen.c
-  ${CMAKE_CURRENT_LIST_DIR}/screens/screen_main_gen.c
-  ${CMAKE_CURRENT_LIST_DIR}/ui_screens_gen.c
-  ${CMAKE_CURRENT_LIST_DIR}/ui_screens.c)
